191a2abae5bb7806b6a8aeafc73a2945e16e065e
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-03-06 21:14:40 +0000

    extended task - extra classes coded

---
+++ JSON/DemoJSON/DemoJSON/CheckoutLibrarian.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DemoJSON
+{
+    public class CheckoutLibrarian : Staff
+    {
+        // Loan a book to a member
+        public void IssueLoan()
+        {
+
+        }
+
+        // Extend / renew a member's loan
+        public void RenewLoan()
+        {
+
+        }
+
+        // Return a member's loan item
+        public void ReturnLoanItem()
+        {
+
+        }
+
+        // Check stock levels
+        public void RunStockReport()
+        {
+
+        }
+    }
+}
---
+++ JSON/DemoJSON/DemoJSON/ChiefLibrarian.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DemoJSON
+{
+    public class ChiefLibrarian : Staff
+    {
+        // Fine a member
+        public void IssueFine()
+        {
+
+        }
+
+        // Collect payment of fine 
+        public void CollectPayment()
+        {
+
+        }
+
+        // Run report of all things important
+        public void RunGeneralReport()
+        {
+
+        }
+    }
+}
