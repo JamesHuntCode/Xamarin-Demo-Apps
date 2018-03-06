a8b56fe777e27c680f651fc7b3a8e810537d2672
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-03-06 21:08:13 +0000

    extended task - core classes coded

---
+++ JSON/DemoJSON/DemoJSON/Member.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DemoJSON
+{
+    public class Member : User
+    {
+        string Address { get; set; }
+        string MemberShipNumber { get; set; }
+
+        // Method to allow members to reserve books
+        public void ReserveItem()
+        {
+
+        }
+    }
+}
---
+++ JSON/DemoJSON/DemoJSON/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DemoJSON
+{
+    public class User
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+    }
+}
