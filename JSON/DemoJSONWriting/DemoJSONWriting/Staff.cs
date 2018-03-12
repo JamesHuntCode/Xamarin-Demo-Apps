f0e54fc183170a88efb02170bfe2b67f677c47ec
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-03-12 15:15:42 +0000

    files & classes created

---
+++ JSON/DemoJSONWriting/DemoJSONWriting/Staff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DemoJSONWriting
+{
+    public class Staff
+    {
+        public Role Role { get; set; }
+        public bool Volunteer { get; set; }
+    }
+}
